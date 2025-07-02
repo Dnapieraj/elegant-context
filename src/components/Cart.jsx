74df87e3c1472509a4a9497a164295caf5056e11
Daniel <d.napieraj06@gmail.com>
2025-07-02 22:26:04 +0200

    mplement CartContext with useReducer to manage the shopping cart state and you are done with creating the app

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,8 @@
 import { CartContext } from '../store/shopping-cart-context'
-import { use } from 'react'
+import { useContext } from 'react'
 
-export default function Cart({ onUpdateItemQuantity }) {
-	const { items } = use(CartContext)
-
+export default function Cart() {
+	const { items, updateItemQuantity } = useContext(CartContext)
 	const totalPrice = items.reduce((acc, item) => acc + item.price * item.quantity, 0)
 	const formattedTotalPrice = `$${totalPrice.toFixed(2)}`
 
@@ -14,7 +13,6 @@ export default function Cart({ onUpdateItemQuantity }) {
 				<ul id="cart-items">
 					{items.map(item => {
 						const formattedPrice = `$${item.price.toFixed(2)}`
-
 						return (
 							<li key={item.id}>
 								<div>
@@ -22,9 +20,9 @@ export default function Cart({ onUpdateItemQuantity }) {
 									<span> ({formattedPrice})</span>
 								</div>
 								<div className="cart-item-actions">
-									<button onClick={() => onUpdateItemQuantity(item.id, -1)}>-</button>
+									<button onClick={() => updateItemQuantity(item.id, -1)}>-</button>
 									<span>{item.quantity}</span>
-									<button onClick={() => onUpdateItemQuantity(item.id, 1)}>+</button>
+									<button onClick={() => updateItemQuantity(item.id, 1)}>+</button>
 								</div>
 							</li>
 						)
